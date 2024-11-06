e9a3284080f771812523410d0556a9bac15b679d
forXuyx <xuyuxiang22@mails.ucas.ac.cn>
2024-11-06 15:17:57 +0800

    add

---
+++ common.h
@@ -0,0 +1,120 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <cublasLt.h>
+#include <float.h>
+
+#define OFFSET(row, col, ld) ((row) * (ld) + (col))
+
+// 取整函数
+template<class T>
+__host__ __device__ T ceil_div(T dividend, T divisor) {
+    return (dividend + divisor-1) / divisor;
+}
+
+// cpu版本的矩阵乘法
+void cpuGemm (float *a, float *b, float *c, const int M, const int N, const int K) {
+
+    for (int m = 0; m < M; m ++ ) {
+        for (int n = 0; n < N; n ++ ) {
+            float sum = 0;
+            for (int k = 0; k < K; k ++ ) {
+                sum += a[OFFSET(m, k, K)] * b[OFFSET(k, n, N)];
+            }
+            c[OFFSET(m, n, N)] = sum;
+        }
+    }
+
+}
+
+
+// -----------------------------------
+// cuda测试套件
+
+// 测试与cpu版本的矩阵乘法的最大误差
+float testMaxError(
+    void (*gpuGemm) (float *, float *, float *, const int, const int, const int),
+    dim3 gridDim, dim3 blockDim, const int M, const int N, const int K) {
+
+    size_t size_a = M * K * sizeof(float);
+    size_t size_b = K * N * sizeof(float);
+    size_t size_c = M * N * sizeof(float);
+
+    float *h_a, *h_b, *h_c, *d_a, *d_b, *d_c, *h_d_c;
+    h_a = (float *)malloc(size_a);
+    h_b = (float *)malloc(size_b);
+    h_c = (float *)malloc(size_c);
+    cudaMalloc(&d_a, size_a);
+    cudaMalloc(&d_b, size_b);
+    cudaMalloc(&d_c, size_c);
+    h_d_c = (float *)malloc(size_c);
+
+    srand(time(0));
+    for (int i = 0; i < M * K; i++)
+        h_a[i] = rand() / float(RAND_MAX);
+    for (int i = 0; i < K * N; i++)
+        h_b[i] = rand() / float(RAND_MAX);
+    cudaMemset(d_c, 15, size_c);
+
+    cpuGemm(h_a, h_b, h_c, M, N, K);
+
+    cudaMemcpy(d_a, h_a, size_a, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, size_b, cudaMemcpyHostToDevice);
+    gpuGemm<<<gridDim, blockDim>>>(d_a, d_b, d_c, M, N, K);
+    cudaMemcpy(h_d_c, d_c, size_c, cudaMemcpyDeviceToHost);
+
+    float max_error = 0.0;
+    for (int i = 0; i < M * N; i++) {
+        float this_error = abs(h_d_c[i] - h_c[i]);
+        if (max_error != max_error || this_error != this_error) // nan
+            max_error = -NAN;
+        else
+            max_error = max(max_error, this_error);
+    }
+
+    free(h_a);
+    free(h_b);
+    free(h_c);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    free(h_d_c);
+
+    return max_error;
+}
+
+float testPerformance(
+    void (*gpuGemm) (float *, float *, float *, const int, const int, const int),
+    dim3 gridDim, dim3 blockDim, const int M, const int N, const int K, const int repeat) {
+
+    size_t size_a = M * K * sizeof(float);
+    size_t size_b = K * N * sizeof(float);
+    size_t size_c = M * N * sizeof(float);
+
+    float *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, size_a);
+    cudaMalloc(&d_b, size_b);
+    cudaMalloc(&d_c, size_c);
+
+    cudaEvent_t start, end;
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+    cudaEventRecord(start);
+    for (int i = 0; i < repeat; i++)
+        gpuGemm<<<gridDim, blockDim>>>(d_a, d_b, d_c, M, N, K);
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+
+    float msec, sec;
+    cudaEventElapsedTime(&msec, start, end);
+    sec = msec / 1000.0 / repeat;
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return sec;
+}
+
+// -----------------------------------
